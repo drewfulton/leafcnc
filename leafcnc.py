b65cbfa7dff5bfd1b426f960b4c1913810a029f1
Drew Fulton <drew@drewfulton.com>
2019-01-03 14:19:37 -0500

    Create leafcnc.py
    Created leafcnc.py and outlined initial code structure

---
+++ leafcnc.py
@@ -0,0 +1,27 @@
+# LeafCNC Application
+
+# Import Libraries and Modules
+
+# Functions to Move CNC Head
+
+# Functions to Control Camera
+
+# Create Config File and Variables
+
+# XML Management Functions
+
+
+# Tkinter Application Overview
+
+# Start Page Class
+
+# Settings Class
+
+# Item Details Class
+
+# Initilization class
+
+# Run Sample Class
+
+#RunApplication Start
+
