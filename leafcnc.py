bb901b84713d9824f8f4c3496413db6485c87781
Drew Fulton <drew@drewfulton.com>
2019-01-05 20:42:00 -0500

    Update leafcnc.py

--- leafcnc.py
+++ leafcnc.py
@@ -304,7 +304,7 @@ class StartPage(tkinter.Frame):
 		btnSettings.grid(row=10, column=14, sticky="NEWS")
 		
 
-		btnQuit = ttk.Button(self, text="Quit", command=lambda: controller.quitProgram(machine))
+		btnQuit = ttk.Button(self, text="Quit", command=lam	bda: controller.quitProgram(machine))
 		btnQuit.grid(row=100, column=6, sticky="EW")
 
 		def startSessionThreading(sessionStatus):
@@ -504,9 +504,9 @@ class StartPage(tkinter.Frame):
 			events["xmlPathProblem"].set()
 			events["pause"].set()
 			while events["pause"].is_set():
-			if events["cancel"].is_set():
-				cancelSession()
-				break
+				if events["cancel"].is_set():
+					cancelSession()
+					break
 			status["xmlpathInit"] = True
 		elif os.path.ismount(config["filepaths"]["xmlPath"]):
 			status["xmlpathInit"] = True
