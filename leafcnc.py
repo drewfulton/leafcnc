c2e6bded64559643c1e19a27215272277ee7e7b2
Drew Fulton <drew@drewfulton.com>
2019-01-21 16:10:48 -0500

    Update leafcnc.py

--- leafcnc.py
+++ leafcnc.py
@@ -1767,7 +1767,7 @@ config = getConfig(configpath)
 machine = openCNC(config["cnc"]["port"])
 xmlData = ET.Element("data")
 xmlTree = ET.ElementTree(xmlData)
-# cameraDatabase = getCameraDatabase()
+cameraDatabase = getCameraDatabase()
 
 #RunApplication Start
 app = LeafCNC()
