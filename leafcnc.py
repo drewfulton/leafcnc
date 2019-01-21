b9432d62075ea66789993266535b5632586b1709
Drew Fulton <drew@drewfulton.com>
2019-01-21 15:16:19 -0500

    Update leafcnc.py

--- leafcnc.py
+++ leafcnc.py
@@ -1376,7 +1376,7 @@ class Settings(tkinter.Frame):
 
 		# Camera Settings
 		lblCameraBody = ttk.Label(self, text="Camera Body", font=MED_FONT)
-		cmbCameraBody = ttk.Combobox(self, textvariable=self.cameraBody, width=10, command=updateLens)
+		cmbCameraBody = ttk.Combobox(self, textvariable=self.cameraBody, width=10, command=lambda: [updateLens()])
 		cmbCameraBody['values'] = list(cameraDatabase.keys())
 		lblCameraBody.grid(row=10, column=10, sticky="WE")
 		cmbCameraBody.grid(row=10, column=11, sticky="WE")
