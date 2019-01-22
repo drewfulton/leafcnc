bf71bc175f5dd784f912568efddecfb9a6617be8
Drew Fulton <drew@drewfulton.com>
2019-01-21 19:49:55 -0500

    Update leafcnc.py

--- leafcnc.py
+++ leafcnc.py
@@ -510,11 +510,11 @@ def saveCameraDatabase(cameraDatabase):
 def getCameraDatabase():
 	filepath = os.path.dirname(os.path.abspath(__file__))+"/backend/cameraDatabase.txt"
 	with open(filepath, 'rb') as f:
-		print(str(f.read()))
 		if not str(f.read()) == "b''":
 			cameraDatabase = pickle.load(f)
 		else:
 			cameraDatabase = {}
+	print("CameraDatabase: "+str(cameraDatabase))
 	return cameraDatabase
 
 # Tkinter Application Overview
