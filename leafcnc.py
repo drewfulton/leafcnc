c3f70f962ca5e36ca5f0bacd53f16778e4d9b2d8
Drew Fulton <drew@drewfulton.com>
2019-01-29 09:50:36 -0500

    Update leafcnc.py

--- leafcnc.py
+++ leafcnc.py
@@ -348,6 +348,7 @@ def playSound(file):
 
 def playSoundThread(sound):
 	pygame.mixer.init()
+	print("Playing Sound: "+str(sound))
 	pygame.mixer.music.load("backend/soundeffects/"+sound+".mp3")
 	pygame.mixer.music.play()
 	time.sleep(3)
@@ -1268,6 +1269,42 @@ class StartPage(tkinter.Frame):
 		pos["x"] = float(config["sample"]["sizex"])
 		pos["y"] = float(config["sample"]["sizey"])
 		positions.append(pos)
+
+		if config["sample"]["stackingMode"] == "Auto":
+			stackCount = 1
+			while stackCount <= int(config["sample"]["stackingCount"]):
+				for position in positions:
+					distanceToTravel = math.sqrt((xPos-int(position["x"]))**2 + (yPos - int(position["y"]))**2)
+			
+					timetoTravel = distanceToTravel/rateOfTravel
+					responseString = moveCNCtoCoordinates(position["x"], position["y"], machine)	
+					time.sleep(timetoTravel)
+					time.sleep(float(config["cnc"]["pause"]))
+
+					sessionStatus.set("Capturing Image #"+str(stackCount)+"/"+str(config["sample"]["stackingCount"])+" at Position #"+str(positionCount)+" of "+str(len(positions)))
+					cameraInfo = triggerImageUSB()
+					finalFilename = str(config["sample"]["id"])+"-"+str(config["sample"]["datestamp"])+"-"+str(imageCount).zfill(3)+str(cameraInfo.name[-4:])
+					imageList.append((cameraInfo.folder+"/"+cameraInfo.name, finalFilename))
+					time.sleep(float(config["camera"]["exposure"]))
+					imageCount +=1	
+					xmlTree = xmlAddImage(position, cameraInfo, finalFilename, stackCount)
+					if events["cancel"].is_set():
+						cancelSession()
+						break
+				stackCount += 1
+				if events["cancel"].is_set():
+					cancelSession()
+					break
+				
+				# move focus farther one step
+				moveFocusFarther(config["cnc"]["stackingSize"])
+			sessionStatus.set("Resetting Focus at Position #"+str(positionCount)+" of "+str(len(positions)))
+			while stackCount > 1:
+				moveFocusCloser(config["cnc"]["stackingSize"])
+				stackCount -= 1
+			positionCount +=1		
+
+
 		
 		for position in positions:
 			sessionStatus.set("Capturing Image at Position #"+str(positionCount)+" of "+str(len(positions)))
@@ -1289,29 +1326,6 @@ class StartPage(tkinter.Frame):
 				if events["cancel"].is_set():
 					cancelSession()
 					break
-			elif config["sample"]["stackingMode"] == "Auto":
-				stackCount = 1
-				while stackCount <= int(config["sample"]["stackingCount"]):
-					sessionStatus.set("Capturing Image #"+str(stackCount)+"/"+str(config["sample"]["stackingCount"])+" at Position #"+str(positionCount)+" of "+str(len(positions)))
-					cameraInfo = triggerImageUSB()
-					finalFilename = str(config["sample"]["id"])+"-"+str(config["sample"]["datestamp"])+"-"+str(imageCount).zfill(3)+str(cameraInfo.name[-4:])
-					imageList.append((cameraInfo.folder+"/"+cameraInfo.name, finalFilename))
-					time.sleep(float(config["camera"]["exposure"]))
-					imageCount +=1	
-					xmlTree = xmlAddImage(position, cameraInfo, finalFilename, stackCount)
-					stackCount += 1
-					if events["cancel"].is_set():
-						cancelSession()
-						break
-					
-					# move focus farther one step
-					moveFocusFarther(config["cnc"]["stackingSize"])
-				sessionStatus.set("Resetting Focus at Position #"+str(positionCount)+" of "+str(len(positions)))
-				while stackCount > 1:
-					moveFocusCloser(config["cnc"]["stackingSize"])
-					stackCount -= 1
-				positionCount +=1		
-
 
 			elif config["sample"]["stackingMode"] == "Manual":
 				# Launch Live View/Manual Window
